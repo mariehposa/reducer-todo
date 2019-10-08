f37fb54f5e55048d80a2bcae4488a78d4c268a41
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-08 16:31:58 +0100

    create todoList

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Reducer from './reducers/Reducer';
+import Todolist from './components/TodoList'
 
 function App() {
   return (
     <div className="App">
-      
+      <Todolist initialStateObject={initila} />
     </div>
   );
 }
--- todo/src/reducers/Reducer.js
+++ todo/src/reducers/Reducer.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-export const initialObject = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
-};
+export const initialObject = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    }
+];
 
-export default function reducer() {
+export default function reducer(state, action) {
+    switch(action.type){
 
+        default: return state;
+    }
 }
