28c509fb8ee7c69c20b3ab7a778d0302c43e10e1
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-08 16:00:11 +0100

    Build a simple reducer and initial state

---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const initialObject = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+};
+
+export default function reducer() {
+
+}
